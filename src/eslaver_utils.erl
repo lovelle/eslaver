0bface2ee5a706ee7746ff3884d603495fa5542e
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2016-09-13 15:25:35 -0300

    basic changes

--- src/eslaver_utils.erl
+++ src/eslaver_utils.erl
@@ -1,20 +1,24 @@
 -module (eslaver_utils).
 
--export ([i2l/1, i2b/1, b2l/1, l2b/1]).
+-export ([i2b/1, i2l/1, b2l/1, b2i/1, l2b/1]).
 
 
-%% Short func for 'integer_to_list'
+%% Shortcut fun for 'integer_to_binary'
+i2b(I) when is_integer(I) ->
+    l2b(i2l(I)).
+
+%% Shortcut fun for 'integer_to_list'
 i2l(I) when is_integer(I) ->
     integer_to_list(I).
 
-%% Short func for 'integer_to_binary'
-i2b(I) when is_integer(I) ->
-    list_to_binary(integer_to_list(I)).
-
-%% Short func for 'binary_to_list'
+%% Shortcut fun for 'binary_to_list'
 b2l(B) when is_binary(B) ->
     binary_to_list(B).
 
-%% Short func for 'list_to_binary'
+%% Shortcut fun for 'binary_num_to_integer'
+b2i(B) when is_binary(B) ->
+    list_to_integer(b2l(B)).
+
+%% Shortcut fun for 'list_to_binary'
 l2b(L) when is_list(L) ->
     list_to_binary(L).
